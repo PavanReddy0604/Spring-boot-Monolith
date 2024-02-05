f39b6c790072b35e5fc0558bbf0646aef33365d6
Pavan <pavankumarreddy977@gmail.com>
2024-02-05 23:53:28 +0530

    intial commit

---
+++ src/main/java/com/app/entity/IdProof.java
@@ -0,0 +1,48 @@
+package com.app.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import org.springframework.lang.NonNull;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="id_proof")
+public class IdProof {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private UUID proofId;
+    @Column(unique = true,nullable = false)
+    @NonNull
+    private String proofName;
+
+    public IdProof(int id, UUID proofId, String proofName) {
+        this.id = id;
+        this.proofId = proofId;
+        this.proofName = proofName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UUID getProofId() {
+        return proofId;
+    }
+
+    public void setProofId(UUID proofId) {
+        this.proofId = proofId;
+    }
+
+    public String getProofName() {
+        return proofName;
+    }
+
+    public void setProofName(String proofName) {
+        this.proofName = proofName;
+    }
+}
