d3d520069d96a95a9ddecca94ec788390a4de623
Pavan <pavankumarreddy977@gmail.com>
2024-02-07 20:41:59 +0530

    Created Project entity

--- src/main/java/com/app/entity/Person.java
+++ src/main/java/com/app/entity/Person.java
@@ -4,6 +4,8 @@ import com.app.util.Gender;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -18,6 +20,89 @@ public class Person extends Audit implements Serializable {
     private Long mobileNumber;
     @OneToOne
     private IdProof proof;
+    @OneToMany(mappedBy = "person")
+    private Set<Project> project;
 
+    public Person(){
+    }
 
+    public Person(LocalDateTime createdAt, LocalDateTime updatedAt, int id, UUID personId, String personName, Gender gender, Long mobileNumber, IdProof proof, Set<Project> project) {
+        super(createdAt, updatedAt);
+        this.id = id;
+        this.personId = personId;
+        this.personName = personName;
+        this.gender = gender;
+        this.mobileNumber = mobileNumber;
+        this.proof = proof;
+        this.project = project;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UUID getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(UUID personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(Long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public IdProof getProof() {
+        return proof;
+    }
+
+    public void setProof(IdProof proof) {
+        this.proof = proof;
+    }
+
+    public Set<Project> getProject() {
+        return project;
+    }
+
+    public void setProject(Set<Project> project) {
+        this.project = project;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", personId=" + personId +
+                ", personName='" + personName + '\'' +
+                ", gender=" + gender +
+                ", mobileNumber=" + mobileNumber +
+                ", proof=" + proof +
+                ", project=" + project +
+                '}';
+    }
 }
